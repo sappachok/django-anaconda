ad07c02ceabffbf6036731a78b2ebc6f754f4df3
sappachok <suppachok_sin@nstru.ac.th>
2019-07-09 10:17:12 +0700

    .

--- app/hello_world/views.py
+++ app/hello_world/views.py
@@ -5,11 +5,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # %matplotlib inline
 
-df = pd.read_excel('data_sheets/UOC_STAFF.xls')
+app_dir = os.path.abspath(os.path.dirname(__file__))
+# df = pd.read_excel('data_sheets/titanic.xls')
 
+# df = pd.read_csv('D:/github-repo/django-datasci/app/hello_world/data_sheets/titanic.csv')
 
 # Create your views here.
 def hello_world(request):
-    path = os.path.abspath(os.path.dirname(__file__))
-    content = {'blog_title':'my first app', 'person':df, 'path':path}
+    path = app_dir
+    debug = ''
+    df = pd.read_csv(os.path.join(app_dir, 'data_sheets','titanic.csv'))
+    content = {'blog_title':'my first app', 'person':df, 'path':path, 'debug':debug}
     return render(request, 'hello_world.html', content)
+
+def print_datalist
