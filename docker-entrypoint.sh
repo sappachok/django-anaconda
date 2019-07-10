c44c9d9779664c678fc859b03b3766159f30aa58
sappachok <suppachok_sin@nstru.ac.th>
2019-07-11 06:46:58 +0700

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# python manage.py migrate — noinput
+gunicorn --workers=2 --bind=0.0.0.0:8000 mysite.wsgi:application
+python manage.py migrate
 # python manage.py collectstatic — noinput
-# gunicorn --workers=2 --bind=0.0.0.0:8000 mysite.wsgi:application
