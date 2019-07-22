8177a626284c31aaae914731df9d1a000dd5c790
sappachok <suppachok_sin@nstru.ac.th>
2019-07-22 15:59:25 +0700

    .

--- app/editor/views.py
+++ app/editor/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from .forms import SampleForm
 
 class BasicSampleFormView(FormView):
-    template_name = 'form.html'
+    #template_name = 'form.html'
+    template_name = 'widget.html'
     form_class = SampleForm
 
     def get_success_url(self):
