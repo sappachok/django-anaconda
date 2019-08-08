df6126f19a2a083c5fce982c4fbc455167f83c29
sappachok <suppachok_sin@nstru.ac.th>
2019-08-08 20:07:37 +0700

    update requirement

--- app/editor/urls.py
+++ app/editor/urls.py
@@ -4,9 +4,9 @@ from editor.views import BasicSampleFormView
 from django.urls import reverse
 
 urlpatterns = [
-    #url('', TemplateView.as_view(
-    #    template_name="editor.html"
-    #), name='editor'),
+    url('', TemplateView.as_view(
+        template_name="view.html"
+    ), name='editor'),
     url('/form/$', BasicSampleFormView.as_view(
         template_name="form.html"
     ), name='codemirror-form'),
